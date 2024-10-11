45555ce5d080dd67fa5c58a1b56076ce71335725
ywgi <ywgi@outlook.com>
2024-10-10 23:31:15 -0500

    inventory page skeleton

---
+++ frontend/pages/inventory/InventoryPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './InventoryPage.css';
+//import components
+
+const InventoryPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default InventoryPage;
