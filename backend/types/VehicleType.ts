172edbaeb8562c9f2c9001a2afb37c064733aa4c
ywgi <ywgi@outlook.com>
2024-10-20 15:19:58 -0500

    changed csv parser

---
+++ backend/types/VehicleType.ts
@@ -0,0 +1,21 @@
+type Vehicle = {
+    vin: string;
+    title: string;
+    make: string;
+    model: string;
+    year: string;
+    mileage: string;
+    transmission: string;
+    fuelType: string;
+    bodyStyle: string;
+    drivetrain: string;
+    stateOfVehicle: string;
+    stockNumber: string;
+    price: string;
+    exteriorColor: string;
+    daysOnLot: string;
+    msrp?: string;
+    images: string[];
+}
+
+export default Vehicle;
--- backend/utilities/vehicle/extractVehicles.ts
+++ backend/utilities/vehicle/extractVehicles.ts
@@ -1,66 +1,66 @@
 
 
 
-const extractVehicles = async () => {
-    const payload = await fetch("https://vendordownloads.homenetinc.com/VerHoefChevroletBuick/VerHoefChevroletBuick.csv");
-    const csvData = await payload.text();
+// const extractVehicles = async () => {
+//     const payload = await fetch("https://vendordownloads.homenetinc.com/VerHoefChevroletBuick/VerHoefChevroletBuick.csv");
+//     const csvData = await payload.text();
     
-    const parseCsv = (csv: string) => {
-      const cars = [];
-      const usefulKeys = [
-        "vin",
-        "title",
-        "url",
-        "make",
-        "model",
-        "year",
-        "mileage.value",
-        "mileage.unit",
-        "image",
-        "transmission",
-        "fuel_type",
-        "body_style",
-        "drivetrain",
-        "state_of_vehicle",
-        "stock_number",
-        "exterior_color",
-        "days_on_lot",
-        "msrp",
-        "price"
-      ];
+//     const parseCsv = (csv: string) => {
+//       const cars = [];
+//       const usefulKeys = [
+//         "vin",
+//         "title",
+//         "url",
+//         "make",
+//         "model",
+//         "year",
+//         "mileage.value",
+//         "mileage.unit",
+//         "image",
+//         "transmission",
+//         "fuel_type",
+//         "body_style",
+//         "drivetrain",
+//         "state_of_vehicle",
+//         "stock_number",
+//         "exterior_color",
+//         "days_on_lot",
+//         "msrp",
+//         "price"
+//       ];
   
-      const [headerLine, ...lines] = csv.trim().split('\n');
-      const headers = headerLine.replaceAll('"', '').split(",");
-      const carRows = [];
-      for (const carRow of lines) {
-        const cleanedCarRow = carRow
-          .replace('"', '')
-          .replace('"\r', '')
-          .split('","');
-        carRows.push(cleanedCarRow);
-      }
+//       const [headerLine, ...lines] = csv.trim().split('\n');
+//       const headers = headerLine.replaceAll('"', '').split(",");
+//       const carRows = [];
+//       for (const carRow of lines) {
+//         const cleanedCarRow = carRow
+//           .replace('"', '')
+//           .replace('"\r', '')
+//           .split('","');
+//         carRows.push(cleanedCarRow);
+//       }
   
-      for (let row = 0; row < carRows.length; row++) {
-        const car = { images: [] };
-        for (let i = 0; i < headers.length; i++) {
-          const header = headers[i].toLowerCase();
-          if (header.includes("image")) {
-            car.images.push(carRows[row][i]);
-            continue;
-          }
-          if (usefulKeys.includes(header)) {
-            car[header] = carRows[row][i];
-          }
-        }
-        cars.push(car);
-      }
+//       for (let row = 0; row < carRows.length; row++) {
+//         const car = { images: [] };
+//         for (let i = 0; i < headers.length; i++) {
+//           const header = headers[i].toLowerCase();
+//           if (header.includes("image")) {
+//             car.images.push(carRows[row][i]);
+//             continue;
+//           }
+//           if (usefulKeys.includes(header)) {
+//             car[header] = carRows[row][i];
+//           }
+//         }
+//         cars.push(car);
+//       }
   
-      return cars;
-    }
+//       return cars;
+//     }
   
-    const parsedData = parseCsv(csvData);
+//     const parsedData = parseCsv(csvData);
     
-    return parsedData;
-};
+//     return parsedData;
+// };
 
-export default extractVehicles;
+// export default extractVehicles;
