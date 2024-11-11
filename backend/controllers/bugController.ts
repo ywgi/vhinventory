153d737374288944926e21be31b1f340cff45a41
ywgi <ywgi@outlook.com>
2024-11-11 13:11:28 -0600

    Controller updates.

--- backend/controllers/bugController.ts
+++ backend/controllers/bugController.ts
@@ -1,2 +1,10 @@
 import Bug from '../models/bugModel';
 import { Request, Response } from 'express';
+
+export const submitBug = async (req: Request, res: Response) =>  {
+
+}
+
+export const getBugs = async (req: Request, res: Response) => {
+    
+}
--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -3,5 +3,13 @@ import { Request, Response } from 'express';
 import { errorResponse } from '../utilities/errorResponse';
 
 export const getUserProfile = async(req: Request, res : Response) => {
-
+    try  {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        if (!user) {
+            res.status(400).json(user);
+        }
+    } catch (error) {
+        res.status(500).json()
+    }
 }
